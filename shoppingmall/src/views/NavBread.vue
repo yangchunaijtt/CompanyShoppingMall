07c88f08ac81b5ec2d2f1c3e2efee318aaa156ad
yangchun <37919689+yangchunaijtt@users.noreply.github.com>
2019-02-12 16:38:40 +0800

    1

---
+++ shoppingmall/src/views/NavBread.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="nav-breadcrumb-wrap">
+        <div class="container">
+            <nav class="nav-breadcrumb">
+            <a href="/">Home</a>
+            <!-- slot插槽，供其他页面来放内容进去 -->
+            <slot name="bread"></slot>
+            </nav>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
