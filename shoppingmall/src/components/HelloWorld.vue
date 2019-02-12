eca4b21eac1135513eb9b4906409230d70c82ce4
yangchun <37919689+yangchunaijtt@users.noreply.github.com>
2019-02-12 14:26:32 +0800

    1

---
+++ shoppingmall/src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>新的vue文件</div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+
+</style>
