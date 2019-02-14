b648beb38fcdd3b45e5a1bfc86d2b5b5248f8ec4
yangchun <37919689+yangchunaijtt@users.noreply.github.com>
2019-02-14 13:56:13 +0800

    2

--- shoppingmall/src/main.js
+++ shoppingmall/src/main.js
@@ -3,9 +3,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueLazyload from 'vue-lazyload'
 
 Vue.config.productionTip = false
-
+Vue.use(VueLazyload,{
+  loading:"/static/loading-svg/loading-bars.svg"
+});
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
--- shoppingmall/src/views/GoodList.vue
+++ shoppingmall/src/views/GoodList.vue
@@ -7,30 +7,36 @@
         <div class="accessory-result-page accessory-page">
             <div class="container">
                 <div class="filter-nav">
-                <span class="sortby">Sort by:</span>
-                <a href="javascript:void(0)" class="default cur">Default</a>
-                <a href="javascript:void(0)" class="price">Price <svg class="icon icon-arrow-short"><use xlink:href="#icon-arrow-short"></use></svg></a>
-                <a href="javascript:void(0)" class="filterby stopPop">Filter by</a>
+                    <span class="sortby">Sort by:</span>
+                    <a href="javascript:void(0)" class="default cur">Default</a>
+                    <a href="javascript:void(0)" class="price">Price <svg class="icon icon-arrow-short"><use xlink:href="#icon-arrow-short"></use></svg></a>
+                    <a href="javascript:void(0)" class="filterby stopPop" @click="showFilterPop">Filter by</a>
                 </div>
                 <div class="accessory-result">
                 <!-- filter -->
-                <div class="filter stopPop" id="filter">
+                <div class="filter stopPop" id="filter" v-bind:class="{'filterby-show':filterBy}">
                     <dl class="filter-price">
                     <dt>Price:</dt>
-                    <dd><a href="javascript:void(0)">All</a></dd>
-                    <dd v-for="item in priceFilter">
-                        <a href="javascript:void(0)">{{item.statePrice}} - {{item.endPrice}}</a>
+                    <dd>
+                        <a href="javascript:void(0)" v-bind:class="{'cur':priceCheck=='all'}" @click = "setPriceFilter('all')">All</a>
+                    </dd>
+                    <!-- 循环读取出价格的区间 -->
+                    <dd v-for="(item,index) in priceFilter" >
+                        <a href="javascript:void(0)" @click = "setPriceFilter(index)"  v-bind:class="{'cur':priceCheck==index}">{{item.startPrice}} - {{item.endPrice}}</a>
                     </dd>
                     </dl>
                 </div>
-
+       
                 <!-- search result accessories list -->
                 <div class="accessory-list-wrap">
                     <div class="accessory-list col-4">
                     <ul>
+                        <!-- 循环读取出购物车和图片的内容 -->
                         <li v-for="(item,index) in goodList">
                             <div class="pic">
-                                <a href="#"><img :src="'/static/'+item.productImg" alt=""></a>
+                                <a href="#">
+                                    <img v-lazy="'/static/'+item.productImg" alt="">
+                                </a>
                             </div>
                             <div class="main">
                                 <div class="name">{{item.productName}}</div>
@@ -46,6 +52,7 @@
                 </div>
             </div>
         </div>
+        <div class="md-overlay" v-show="overLayFlag" @click="closePop"></div>
         <nav-footer></nav-footer>
     </div>
 </template>
@@ -62,22 +69,25 @@
                 goodList:[],
                 priceFilter:[
                     {
-                        statePrice:"0.00",
+                        startPrice:"0.00",
                         endPrice:"100.00"
                     },
                     {
-                        statePrice:"100.00",
+                        startPrice:"100.00",
                         endPrice:"500.00"
                     },
                     {
-                        statePrice:"500.00",
+                        startPrice:"500.00",
                         endPrice:"1000.00"
                     },
                     {
-                        statePrice:"1000.00",
+                        startPrice:"1000.00",
                         endPrice:"2000.00"
                     }
-                ]
+                ],
+                priceCheck:"all",
+                filterBy:false,
+                overLayFlag:false,
             }
         },
         components:{
@@ -94,6 +104,21 @@
                     var res = result.data;
                     this.goodList = res.result;
                 });
+            },
+            // 控制弹出
+            showFilterPop(){
+                this.filterBy = true;
+                this.overLayFlag = true;
+            },
+            // 控制隐藏
+            closePop(){
+                this.filterBy = false;
+                this.overLayFlag = false;
+            },
+            setPriceFilter(index){
+                this.priceCheck = index;
+                this.filterBy = false;
+                this.overLayFlag = false;
             }
         }
     }
